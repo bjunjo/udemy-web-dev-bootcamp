3c85cf7ceeb1a490dd50036cdbffe09617e0086f
byoungjunjo <byoungjunjo@gmail.com>
2017-04-08 22:47:54 -0700

    Final update

---
+++ to-do-list-project/assets/js/todos.js
@@ -0,0 +1,28 @@
+// Check off specofoc todos by clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+// Click on X to delete todos
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// input controls
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    // grabbing new todo from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+  }
+});
+
+// toggle for the plus sign
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+});
